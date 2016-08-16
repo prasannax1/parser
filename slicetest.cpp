5a42d792d17032b94cda065fa9f52bd79ab97ee9
Prasanna Rao <prasannax1@gmail.com>
2016-08-15 22:32:25 -0400

    working version of slicetest, but does not test all

--- slicetest.cpp
+++ slicetest.cpp
@@ -3,8 +3,11 @@
 
 int main() {
     auto s = Slice{(char *)"hello world"};
-    auto a = s(1,5);
 
-    std::cout << "s = " << std::string(s) << " and a = " << std::string(a) << std::endl;
+    std::cout << "s = " << std::string(s) << std::endl;
+
+    for (auto x: s) {
+        std::cout << x << std::endl;
+    }
 }
 
